8d1dac00892a64e4946e2996288c4f8974901d96
Minh Phuong and Nicole Sanders <minh+nicole@>
2017-04-18 09:46:57 -0700

    add first page for displaying all players and teams

---
+++ src/SportLeague/Controllers/PlayersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportLeague.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportLeague.Controllers
+{
+    public class PlayersController : Controller
+    {
+        // GET: /<controller>/
+        private SportsLeagueContext db = new SportsLeagueContext();
+        public IActionResult Index()
+        {
+
+            return View(db.Players.ToList());
+        }
+
+    }
+}
